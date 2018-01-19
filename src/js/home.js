8e3e9d9693650e53010962c6bbc076a08eee1a11
delalik <delalik@mcmaster.ca>
2018-01-19 14:08:10 -0500

    Update motto

--- src/js/home.js
+++ src/js/home.js
@@ -12,7 +12,7 @@ export default class Home extends Component {
     render() {
       return (
         <div className="home container">
-        	<h1 className="motto">I make great things, and explore ways to push the barrier</h1>
+        	<h1 className="motto">I design and develop awesome solutions for various types of problems.</h1>
         	<NavLink to="/about">
 	        	<RaisedButton style={style} 
 	        	className="learnMore pulse-button"
