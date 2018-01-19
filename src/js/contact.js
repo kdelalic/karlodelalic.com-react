e4f2cd8a4cb42f3844ed67f69f10fc4ba469b841
delalik <delalik@mcmaster.ca>
2018-01-19 14:38:53 -0500

    Fix resume link

--- src/js/contact.js
+++ src/js/contact.js
@@ -19,7 +19,7 @@ export default class Contact extends Component {
           <div className="dash"></div>
           <h3>Here's my RESUMÉ</h3>
           <RaisedButton className="raisedButton"
-            href="../resume/Karlo_Delalic_Resume.pdf"
+            href="https://drive.google.com/open?id=1OjHFmSraRutZExOPxcjrfqFYmVMIhGB_"
             target="_blank"
             label="Resumé"
             primary={true}
